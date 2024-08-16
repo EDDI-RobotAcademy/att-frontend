03796c3cee384e356c28251e86e021942e35718a
HoJ00n2 <81820604+HoJ00n2@users.noreply.github.com>
2024-08-16 17:43:52 +0900

    feat: [이호준] Unity 빌드 파일을 불러옵니다. [ATTF-21]

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,31 @@
 <template>
+  <div id="app">
+    <!-- 기존 컴포넌트 -->
+    <ExistingComponent />
+    
+    <!-- UnityComponent 추가 -->
+    <UnityComponent />
+  </div>
+</template>
+
+<script>
+import ExistingComponent from './components/ExistingComponent.vue';
+import UnityComponent from './components/UnityComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ExistingComponent,
+    UnityComponent
+  }
+};
+</script>
+
+<style>
+/* Global styles here */
+</style>
+
+<!-- <template>
   <v-app>
     <navigation-menu-bar/>
     <v-main>
@@ -12,6 +39,7 @@ import { defineComponent } from 'vue'
 import NavigationMenuBar from './navigationBar/NavigationMenuBar.vue';
 
 export default defineComponent({
+  
   components: { NavigationMenuBar },
   name: 'App',
 
@@ -22,4 +50,4 @@ export default defineComponent({
   },
 })
 </script>
-
+ -->
--- src/components/UnityGame.vue
+++ src/components/UnityGame.vue
@@ -1,4 +1,68 @@
+<!-- src/components/UnityComponent.vue -->
 <template>
+  <div id="unity-container">
+    <div id="unity-canvas"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UnityComponent',
+  mounted() {
+    var buildUrl = "/Build";
+    var loaderUrl = buildUrl + "/IF_TEST.loader.js";
+    var config = {
+        // dataUrl: `${buildUrl}/IF_TEST.data`,
+        dataUrl: buildUrl + "/IF_TEST.data",
+        frameworkUrl: buildUrl + "/IF_TEST.framework.js",
+        codeUrl: buildUrl + "/IF_TEST.wasm",
+        streamingAssetsUrl: "StreamingAssets",
+        companyName: "DefaultCompany",
+        productName: "IF",
+        productVersion: "1.0",
+    };
+
+    const script = document.createElement('script');
+    script.src = loaderUrl;
+    script.onload = () => {
+      createUnityInstance(document.querySelector("#unity-canvas"), config, (progress) => {
+        console.log(`Loading: ${progress * 100}%`);
+      }).then((unityInstance) => {
+        this.unityInstance = unityInstance;
+      }).catch((message) => {
+        console.error(message);
+      });
+    };
+    document.body.appendChild(script);
+  },
+  beforeUnmount() {
+    if (this.unityInstance) {
+      this.unityInstance.Quit();
+    }
+  }
+};
+</script>
+
+<style scoped>
+#unity-container {
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#unity-canvas {
+  width: 100%;
+  height: 100%;
+  outline: none;
+}
+</style>
+
+<!-- <template>
   <div class="unity-container">
     <iframe src="/Unity.html" class="unity-frame"></iframe>
   </div>
@@ -24,4 +88,4 @@ export default {
   height: 100%;
   border: none;
 }
-</style>
+</style> -->
